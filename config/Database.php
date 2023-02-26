7c0cca540fdc4eaa0ec6aea7cfea2520335b349f
p-baldwin <p_baldwin@mail.fhsu.edu>
2023-02-26 16:58:55 -0600

    Modified Database.php. All DB Connection variables come from environment variables.

--- config/Database.php
+++ config/Database.php
@@ -1,22 +1,24 @@
 <?php
     class Database {
         // DB Parameters
-        private $host = 'localhost';
-        private $port = '5432';
-        private $db_name = "";
-        private $username = "";
-        private $password = "";
+        private $host = '';
+        private $port = '';
+        private $db_name = '';
+        private $username = '';
+        private $password = '';
         private $conn;
 
         // DB Connection Function
         public function connect() {
             $this->conn = null;
 
-            // Get the DB name and credentials from the environment
+            // Get the DB host, name, and credentials from the environment
+            $this->host = getenv('HTTP_DBHOST');
+            $this->port = getenv('HTTP_DBPORT');
             $this->db_name = getenv('HTTP_DBNAME');
             $this->username = getenv('HTTP_DBUSER');
             $this->password = getenv('HTTP_DBPASSWORD');
-            
+
             $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name}";
             try {
                 // Create the DB Connection
