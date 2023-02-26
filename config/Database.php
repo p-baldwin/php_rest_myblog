52e361a97b6f08b3baea4447b4f948e61ac17b04
p-baldwin <p_baldwin@mail.fhsu.edu>
2023-02-26 16:48:53 -0600

    Update Database.php to retrieve DB connection details from Environment

--- config/Database.php
+++ config/Database.php
@@ -3,19 +3,22 @@
         // DB Parameters
         private $host = 'localhost';
         private $port = '5432';
-        private $db_name = 'myblog';
-        private $username = 'postgres';
+        private $db_name = "";
+        private $username = "";
         private $password = "";
         private $conn;
 
         // DB Connection Function
         public function connect() {
             $this->conn = null;
+
+            // Get the DB name and credentials from the environment
+            $this->db_name = getenv('HTTP_DBNAME');
+            $this->username = getenv('HTTP_DBUSER');
+            $this->password = getenv('HTTP_DBPASSWORD');
+            
             $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name}";
             try {
-                // Get the password from the environment
-                $this->password = getenv('HTTP_DBPASSWORD');
-                
                 // Create the DB Connection
                 $this->conn = new PDO($dsn, $this->username, $this->password);
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
